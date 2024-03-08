fed39b43ac7883053cdc3f4608dee10da5d642de
alendokofhgr <106598017+alendokofhgr@users.noreply.github.com>
2024-03-08 13:31:30 +0100

    Update supabase.js

--- schritt11/supabase.js
+++ schritt11/supabase.js
@@ -1,8 +1,8 @@
 console.log("Initialisierung Supabase");
 
 // Supabase Initialisierung
-const supabaseUrl = 'https://ughttwxwygsxfulngzdr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnaHR0d3h3eWdzeGZ1bG5nemRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUzMDQ3MDcsImV4cCI6MjAxMDg4MDcwN30.SBN980B1BlVdcBoJR3DCrlLVkyI9w4CpIvE1a12bNaY'
+const supabaseUrl = 'eigene_url_eintragen'
+const supabaseKey = 'eigenen_key_eintragen'
 const supa = supabase.createClient(supabaseUrl, supabaseKey, {
     auth: {
         redirectTo: window.location.origin,  // This will redirect back to the page where the request originated from
